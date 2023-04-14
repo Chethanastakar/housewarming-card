7edd3818920c3b14332079b3f7b7db738c0458b3
Chethan Kumar B <chethanastakar@gmail.com>
2023-04-14 19:52:17 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -46,33 +46,33 @@ var x = setInterval(function() {
 // being a bit cool :p  
 var styles = [
     'background: linear-gradient(#D33106, #571402)'
-    , 'border: 4px solid #3E0E02'
+    , 'border: 3px solid #3E0E02'
     , 'color: white'
     , 'display: block'
     , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 0.3)'
     , 'box-shadow: 0 2px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset'
-    , 'line-height: 20px'
+    , 'line-height: 10px'
     , 'text-align: center'
     , 'font-weight: bold'
-    , 'font-size: 22px'
+    , 'font-size: 15px'
 ].join(';');
 
 var styles1 = [
     'color: #FF6C37'
     , 'display: block'
     , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 1)'
-    , 'line-height: 20px'
+    , 'line-height: 10px'
     , 'font-weight: bold'
-    , 'font-size: 22px'
+    , 'font-size: 15px'
 ].join(';');
 
 var styles2 = [
     'color: teal'
     , 'display: block'
     , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 1)'
-    , 'line-height: 20px'
+    , 'line-height: 10px'
     , 'font-weight: bold'
-    , 'font-size: 22px'
+    , 'font-size: 15px'
 ].join(';');
 
 console.log('\n\n%c SAVE THE DATE: 11th May, 2023!', styles);
