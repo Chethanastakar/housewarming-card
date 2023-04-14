24d157fb1bf4a3ce53054c2fe19f45519d810d5e
Chethan Kumar B <chethanastakar@gmail.com>
2023-04-14 18:50:58 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -61,7 +61,7 @@ var styles1 = [
     'color: #FF6C37'
     , 'display: block'
     , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 1)'
-    , 'line-height: 40px'
+    , 'line-height: 20px'
     , 'font-weight: bold'
     , 'font-size: 32px'
 ].join(';');
@@ -70,7 +70,7 @@ var styles2 = [
     'color: teal'
     , 'display: block'
     , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 1)'
-    , 'line-height: 40px'
+    , 'line-height: 20px'
     , 'font-weight: bold'
     , 'font-size: 32px'
 ].join(';');
@@ -80,6 +80,6 @@ console.log('\n\n%c SAVE THE DATE: 11th May, 2023!', styles);
 console.log('%cYour presence is requested!%c\n\nRegards: Vinit Shahdeo', styles1, styles2);
 
 console.log(
-    `%cShaadi me zaroor aana!\n\n`,
+    `%Ghar ko zaroor aana!\n\n`,
     'color: yellow; background:tomato; font-size: 24pt; font-weight: bold',
 )
